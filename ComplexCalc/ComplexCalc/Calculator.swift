42b5b0c532f3993655fa7cbeb7649506459c3352
TheRealGuopher <jjguo@uw.edu>
2018-01-20 11:16:27 -0800

    Add, multiply, count, avg functions that pass in [Int]

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,6 +10,27 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    public func add(_ arg: [Int]) -> Int {
+        var sum = 0
+        for num in arg {
+            sum += num
+        }
+        return sum
+    }
+    public func multiply(_ arg: [Int]) -> Int {
+        var sum = 1
+        for num in arg {
+            sum *= num
+        }
+        return sum
+    }
+    public func count(_ arg: [Int]) -> Int {
+        return arg.count
+    }
+    public func avg(_ arg: [Int]) -> Int {
+        let sum = add(arg)
+        return sum / arg.count
+    }
     public func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
     }
@@ -21,5 +42,12 @@ class Calculator {
     }
     public func divide(lhs: Int, rhs: Int) -> Int {
         return lhs / rhs
+    }
+    public func multiply(_ arg: [String]) -> Int {
+        var sum = 0
+        for num in arg {
+            sum *= Int(num)!
+        }
+        return sum
     }
 }
