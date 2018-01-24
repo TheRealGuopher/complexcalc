3d79a4762013f3cc849928458f81c60d5f71d76d
TheRealGuopher <jjguo@uw.edu>
2018-01-24 08:35:22 -0800

    Finish mathops assertions

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,12 +10,26 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    public func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var total = beg
+        for num in args {
+            total = op(total, num)
+        }
+        return total
+    }
+    
+    public func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    // tuple functions
     public func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
         return (lhs.0 + rhs.0, lhs.1 + rhs.1)
     }
     public func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
         return (lhs.0 - rhs.0, lhs.1 - rhs.1)
     }
+    // array functions
     public func add(_ arg: [Int]) -> Int {
         var sum = 0
         for num in arg {
@@ -30,6 +44,7 @@ class Calculator {
         }
         return sum
     }
+    // arbitrary functions
     public func count(_ arg: [Int]) -> Int {
         return arg.count
     }
@@ -37,6 +52,7 @@ class Calculator {
         let sum = add(arg)
         return sum / arg.count
     }
+    // simple two number functions
     public func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
     }
